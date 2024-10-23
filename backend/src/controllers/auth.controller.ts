fc2e9d91d1b741806848bc62da5f786171b96b19
Abylai12 <abilay1208@gmail.com>
2024-10-23 11:05:57 +0800

    update

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from "express";
-import User from "../models/user.model";
+import Employee from "../models/employee.model";
 import bcrypt from "bcrypt";
 import { generateToken } from "../utils/generateToken";
-import { generateGmail, sendEmailWithLink } from "../utils/sentGmail";
 import crypto from "crypto";
 
 export const login = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   console.log("first", email);
   try {
-    const user = await User.findOne({ email });
+    const user = await Employee.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Not found user" });
     }
@@ -28,23 +27,24 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 
-export const logup = async (req: Request, res: Response) => {
+export const createEmployee = async (req: Request, res: Response) => {
   try {
-    const { email, firstName, lastName, password } = req.body;
+    const { email, firstName, lastName, password, job_title } = req.body;
 
-    if (!firstName || !lastName || !email || !password) {
+    if (!firstName || !lastName || !email || !password || !job_title) {
       return res.status(400).json({ message: " Хоосон утга байж болохгүй" });
     }
 
     // console.log("first", hashedPassword);
-    const createdUser = await User.create({
+    const createdEmployee = await Employee.create({
       firstName,
       lastName,
       email,
       password,
+      job_title,
     });
 
-    res.status(200).json({ message: "success", user: createdUser });
+    res.status(200).json({ message: "success", employee: createdEmployee });
   } catch (error) {
     res.status(400).json({ message: error });
   }
