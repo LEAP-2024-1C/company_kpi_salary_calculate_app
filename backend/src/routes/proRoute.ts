4b2db0782da1c37dc86ea0a949b8323363f21a67
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-11-02 23:34:00 +0800

    save product model uusgev

--- backend/src/routes/proRoute.ts
+++ backend/src/routes/proRoute.ts
@@ -3,19 +3,16 @@ import {
   createProduct,
   getAllProducts,
   getAllProductsStat,
-
   getCurrentProduct,
-  // getAllProductsStatEmployee,
+  getAllProductsStatEmployee,
 } from "../controllers/product-contoller";
 import { auth } from "../middlewares/auth";
-
 
 const router = Router();
 
 router.route("/pro/product").post(createProduct).get(getAllProducts);
 router.route("/product/stat").get(getAllProductsStat);
-// router.route("/product/stat/employee").get(getAllProductsStatEmployee);
+router.route("/product/stat/employee").get(getAllProductsStatEmployee);
 router.route("/product/:id").get(getCurrentProduct);
-
 
 export default router;
--- backend/src/routes/savedRoute.ts
+++ backend/src/routes/savedRoute.ts
@@ -1,11 +1,13 @@
-// import { Router } from "express";
-// import { auth } from "../middlewares/auth";
-// import { createSavedTasks, getUserSavedTasks, updateSavedTasks } from "../controllers/savedTasks-controller";
+import { Router } from "express";
+import { auth } from "../middlewares/auth";
+import {
+  createSavedTasks,
+  getUserSavedTasks,
+} from "../controllers/savedTasks-controller";
 
-// const router = Router();
+const router = Router();
 
-// router.route("/create-savedTasks").post(createSavedTasks);
-// router.route("/get-savedTaks").get(auth, getUserSavedTasks);
-// router.route("/update-savedTasks").put(auth, updateSavedTasks);
+router.route("/save/employee/task").post(createSavedTasks);
+router.route("/get-savedTaks").get(auth, getUserSavedTasks);
 
-// export default router;
+export default router;
