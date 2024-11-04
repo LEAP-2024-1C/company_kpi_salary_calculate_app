f658950823401cd5cf83dd54569856f0ae8529d9
Амин-Эрдэнэ Харцага <24LU3801@Administrators-iMac-2.local>
2024-11-04 13:07:33 +0800

    save

--- frontend/src/app/[id]/page.tsx
+++ frontend/src/app/[id]/page.tsx
@@ -12,7 +12,7 @@ import { useParams } from "next/navigation";
 import { useProducts } from "@/context/product-provider";
 import axios from "axios";
 import { apiUrl } from "@/lib/utils";
-import { IProduct } from "@/utils/interfaces";
+import { IProduct, ISaveTasks } from "@/utils/interfaces";
 import ProductDetailModal from "@/components/productDetailModal";
 import { toast } from "react-toastify";
 // import { useTasks } from "@/context/task-provider";
@@ -21,6 +21,7 @@ const TaskDetail = () => {
   const { user } = useUser();
   const { id } = useParams();
   const [oneProduct, setOneProduct] = useState<IProduct>();
+  const [savedProductData, setSavedProductData] = useState<ISaveTasks>();
 
   const getCurrentProduct = async () => {
     try {
@@ -30,6 +31,9 @@ const TaskDetail = () => {
         const { oneProductDatas } = res.data;
         console.log("data", oneProductDatas);
         setOneProduct(oneProductDatas);
+        const savedProductData = oneProductDatas.components;
+        setSavedProductData(savedProductData);
+        console.log("comps", savedProductData);
       }
     } catch (error) {
       console.error(error);
