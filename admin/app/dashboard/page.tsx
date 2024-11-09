1158864d2a35c292b795fedd03e8732b153166bc
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-11-09 22:19:10 +0800

    admin can change task status

--- admin/app/dashboard/page.tsx
+++ admin/app/dashboard/page.tsx
@@ -2,20 +2,7 @@
 
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
-import { RecentSales } from '@/components/recent-sales';
 import { ProjectClient } from '@/components/tables/project-tables/client';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card';
-import { toast } from '@/components/ui/use-toast';
-import { IUser } from '@/constants/data';
-import { apiUrl } from '@/lib/utils';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
@@ -23,28 +10,6 @@ const breadcrumbItems = [
 ];
 
 export default function page() {
-  const [projectData, setProjectData] = useState<IUser[]>([]);
-  const getProject = async () => {
-    try {
-      const res = await axios.get(`${apiUrl}auth/get/project`);
-
-      if (res.status === 200) {
-        const { project } = res.data;
-        setProjectData(project);
-        toast({ title: 'successfully get employee' });
-      }
-    } catch (error) {
-      toast({
-        variant: 'destructive',
-        description: 'There was a problem with your request.'
-      });
-    }
-  };
-
-  useEffect(() => {
-    getProject();
-  }, []);
-
   return (
     <PageContainer scrollable={true}>
       <div className="space-y-8">
@@ -55,7 +20,7 @@ export default function page() {
         </div>
         <div className="space-y-4">
           <Breadcrumbs items={breadcrumbItems} />
-          <ProjectClient data={projectData} />
+          <ProjectClient />
         </div>
       </div>
     </PageContainer>
